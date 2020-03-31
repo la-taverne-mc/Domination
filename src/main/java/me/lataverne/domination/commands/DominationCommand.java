8485d378aab21b8e52a3a39713a4bac68bce6639
Po-C4 <leflem.mateo2002@gmail.com>
2020-03-31 12:13:45 +0200

    Ajout de l'auto completion et de la sous-commande createFlag

--- src/main/java/me/lataverne/domination/commands/DominationCommand.java
+++ src/main/java/me/lataverne/domination/commands/DominationCommand.java
@@ -2,53 +2,57 @@ package me.lataverne.domination.commands;
 
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import me.lataverne.domination.Main;
+import me.lataverne.domination.game.Flag;
+import me.lataverne.domination.utils.NumericUtils;
 
 public class DominationCommand implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length >= 1) {
-            switch (args[0]) {
-                //dom new {nom_game} // /dom create {nom_game}
-                case "create":
-                    if (args.length >= 2) {
-                        sender.sendMessage(args[1]);
-                        Main.games.add(args[1]);
-                    } else {
-                        sender.sendMessage("§cIl te manque un argument: /Domination create <name_game>");
+            switch (args[0].toLowerCase()) {
+                case "createflag":
+                    if (!(sender instanceof Player)) {
+                        sender.sendMessage("§cSeul les joueurs peuvent utiliser cette commande");
+                        return false;
                     }
-                    return true;
 
-                case "list":
-                    if (!Main.games.isEmpty()) {
-                        for (String game : Main.games) {
-                            sender.sendMessage(game);
-                        }
-                    } else {
-                        sender.sendMessage("§eIl n'y a pas de games crées pour l'instant");
+                    if (args.length < 2 || args.length > 4) {
+                        sender.sendMessage("§cUsage : /domination createFlag <name> [flag_radius] [bossbar_radius]");
+                        return false;
                     }
+
+                    if (args.length >= 3 && !NumericUtils.isDouble(args[2])) {
+                        sender.sendMessage("§cL'argument 'flag_radius' doit être un nombre valide");
+                        return false;
+                    }
+
+                    if (args.length >= 4 && !NumericUtils.isDouble(args[3])) {
+                        sender.sendMessage("§cL'argument 'bossbar_radius' doit être un nombre valide");
+                        return false;
+                    }
+
+                    double flagRadius;
+                    double bossbarRadius;
+
+                    if (args.length < 3) flagRadius = 3.5;
+                    else flagRadius = Double.parseDouble(args[2]);
+
+                    if (args.length < 4) bossbarRadius = 15;
+                    else bossbarRadius = Double.parseDouble(args[3]);
+
+                    Flag flag = new Flag(args[1], ((Player) sender).getLocation(), flagRadius, bossbarRadius);
+                    flag.activate();
+                    
                     return true;
-                //dom flag {nom_game} : donne une balise, qui créer les drapeau au clic et l'ajout
-//                case "flag":
-//                    if (args.length >= 2) {
-//                        sender.sendMessage(args[1]);
-//                    } else {
-//                        sender.sendMessage("§cIl te manque un argument: /Domination create <name_game>");
-//                    }
-//                    return true;
-//                //dom radius {nom_game} {radius} : défini la taille de la zone des flags
-//                case "radius":
-//
-//                //dom spawnpoint {team} {nom_game} set le spawn point de l'équipe bleu
-//                case "spawnpoint":
-//                ///dom win {nom_game} {score} : défini le score a atteindre pour gagner
-//                case "scorewin":
 
                 default:
                     sender.sendMessage("§cCet argument n'existe pas");
@@ -61,6 +65,16 @@ public class DominationCommand implements TabExecutor {
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        return null;
+        if (!command.getName().equalsIgnoreCase("domination") || args.length != 1) return null;
+
+        List<String> argsList = Lists.newArrayList("createFlag");
+
+        List<String> list = Lists.newArrayList();
+
+        for (String arg : argsList) {
+            if (arg.toLowerCase().startsWith(args[0].toLowerCase())) list.add(arg);
+        }
+
+        return list;
     }
 }
