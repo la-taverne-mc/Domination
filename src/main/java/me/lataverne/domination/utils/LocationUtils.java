7bd637799ebbcd8df9931ace0e20f77e9fba6d79
Po-C4 <leflem.mateo2002@gmail.com>
2020-04-06 08:24:05 +0200

    Ajout de deux méthode pour passer de coordonnées à une chaine de caractères et inversement

--- src/main/java/me/lataverne/domination/utils/LocationUtils.java
+++ src/main/java/me/lataverne/domination/utils/LocationUtils.java
@@ -2,8 +2,10 @@ package me.lataverne.domination.utils;
 
 import com.google.common.collect.Lists;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class LocationUtils {
     public static Location getApproximativeLocation(@NotNull Location location) {
@@ -13,5 +15,26 @@ public class LocationUtils {
         float pitch = NumericUtils.closest(Lists.newArrayList(-90.f, -45.f, 0.f, 45.f, 90.f), location.getPitch());
         
         return new Location(location.getWorld(), x, location.getBlockY(), z, yaw, pitch);
+    }
+
+    public static String LocationToString(@NotNull Location location) {
+        String str = String.join(";",
+            location.getWorld().getUID().toString(),
+            String.valueOf(location.getX()),
+            String.valueOf(location.getY()),
+            String.valueOf(location.getZ()),
+            String.valueOf(location.getYaw()),
+            String.valueOf(location.getPitch())
+        );
+
+        return str;
+    }
+
+    public static @Nullable Location StringToLocation(@NotNull String string) {
+        String[] str = string.split(";");
+
+        if (str.length != 6) return null;
+
+        return new Location(Bukkit.getWorld(str[0]), Double.parseDouble(str[1]), Double.parseDouble(str[2]), Double.parseDouble(str[3]), Float.parseFloat(str[4]), Float.parseFloat(str[5]));
     }
 }
