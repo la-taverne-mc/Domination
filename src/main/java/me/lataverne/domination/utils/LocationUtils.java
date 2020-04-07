ba9068f971d446f1d5a11cfbbe8310be26dc2c5c
Po-C4 <leflem.mateo2002@gmail.com>
2020-04-07 05:24:32 +0200

    Ajout de checks pour verifier la validité des parametres

--- src/main/java/me/lataverne/domination/utils/LocationUtils.java
+++ src/main/java/me/lataverne/domination/utils/LocationUtils.java
@@ -1,5 +1,7 @@
 package me.lataverne.domination.utils;
 
+import java.util.UUID;
+
 import com.google.common.collect.Lists;
 
 import org.bukkit.Bukkit;
@@ -11,14 +13,16 @@ public class LocationUtils {
     public static Location getApproximativeLocation(@NotNull Location location) {
         double x = NumericUtils.closest(Lists.newArrayList(location.getBlockX() - 0.5d, location.getBlockX() + 0.5d), location.getX());
         double z = NumericUtils.closest(Lists.newArrayList(location.getBlockZ() - 0.5d, location.getBlockZ() + 0.5d), location.getZ());
-        float yaw = NumericUtils.closest(Lists.newArrayList(0.f, 90.f, 180.f, 270.f, 360.f), location.getYaw());
+        float yaw = NumericUtils.closest(Lists.newArrayList(0.f, 45.f, 90.f, 135.f, 180.f, 225.f, 270.f, 315.f, 360.f), location.getYaw());
         yaw = yaw == 360.f ? 0.f : yaw;
         float pitch = NumericUtils.closest(Lists.newArrayList(-90.f, -45.f, 0.f, 45.f, 90.f), location.getPitch());
         
         return new Location(location.getWorld(), x, location.getBlockY(), z, yaw, pitch);
     }
 
-    public static String LocationToString(@NotNull Location location) {
+    public static @Nullable String LocationToString(Location location) {
+        if (location == null) return null;
+
         String str = String.join(";",
             location.getWorld().getUID().toString(),
             String.valueOf(location.getX()),
@@ -31,11 +35,13 @@ public class LocationUtils {
         return str;
     }
 
-    public static @Nullable Location StringToLocation(@NotNull String string) {
+    public static @Nullable Location StringToLocation(String string) {
+        if (string == null) return null;
+
         String[] str = string.split(";");
 
-        if (str.length != 6) return null;
+        if (str.length != 6 || !(UuidUtils.isValidUuid(str[0]) && NumericUtils.isDouble(str[1]) && NumericUtils.isDouble(str[2]) && NumericUtils.isDouble(str[3]) && NumericUtils.isDouble(str[4]) && NumericUtils.isDouble(str[5]))) return null;
 
-        return new Location(Bukkit.getWorld(str[0]), Double.parseDouble(str[1]), Double.parseDouble(str[2]), Double.parseDouble(str[3]), Float.parseFloat(str[4]), Float.parseFloat(str[5]));
+        return new Location(Bukkit.getWorld(UUID.fromString(str[0])), Double.parseDouble(str[1]), Double.parseDouble(str[2]), Double.parseDouble(str[3]), Float.parseFloat(str[4]), Float.parseFloat(str[5]));
     }
 }
